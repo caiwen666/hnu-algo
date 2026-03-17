8fd61d3e7067296c60c052b6188325d27a8bc372
caiwen <3102733279@qq.com>
2026-03-17 12:40:07 +0800

    refactor(test/divide_conquer): 将不同数据集上的测试合入到一个函数中

--- tests/divide_conquer.rs
+++ tests/divide_conquer.rs
@@ -2,96 +2,65 @@ use hnu_algo::{algorithms, dataset};
 
 #[test]
 #[ignore]
-fn test_find_min_max_small() {
-    let data = dataset::seq::load_normal_small();
-    let (min, max) = algorithms::divide_conquer::find_min_max(&data);
-    let mut expected_min = 0;
-    let mut expected_max = 0;
-    for (index, value) in data.iter().enumerate() {
-        if value < &data[expected_min] {
-            expected_min = index;
+fn test_find_min_max_all_sizes() {
+    let datasets: Vec<(&str, Vec<usize>)> = vec![
+        ("small", dataset::seq::load_normal_small()),
+        ("medium", dataset::seq::load_normal_medium()),
+        ("large", dataset::seq::load_normal_large()),
+    ];
+
+    for (name, data) in datasets {
+        println!("testing find_min_max, dataset: {}", name);
+
+        let (min, max) = algorithms::divide_conquer::find_min_max(&data);
+        let mut expected_min = 0;
+        let mut expected_max = 0;
+
+        for (index, value) in data.iter().enumerate() {
+            if value < &data[expected_min] {
+                expected_min = index;
+            }
+            if value > &data[expected_max] {
+                expected_max = index;
+            }
         }
-        if value > &data[expected_max] {
-            expected_max = index;
-        }
+
+        assert_eq!(
+            min, expected_min,
+            "find_min_max on {} dataset has incorrect minimum index",
+            name
+        );
+        assert_eq!(
+            max, expected_max,
+            "find_min_max on {} dataset has incorrect maximum index",
+            name
+        );
     }
-    assert_eq!(min, expected_min);
-    assert_eq!(max, expected_max);
 }
 
 #[test]
 #[ignore]
-fn test_find_min_max_medium() {
-    let data = dataset::seq::load_normal_medium();
-    let (min, max) = algorithms::divide_conquer::find_min_max(&data);
-    let mut expected_min = 0;
-    let mut expected_max = 0;
-    for (index, value) in data.iter().enumerate() {
-        if value < &data[expected_min] {
-            expected_min = index;
-        }
-        if value > &data[expected_max] {
-            expected_max = index;
-        }
+fn test_sort_all_sizes() {
+    let datasets: Vec<(&str, Vec<usize>)> = vec![
+        ("small", dataset::seq::load_normal_small()),
+        ("medium", dataset::seq::load_normal_medium()),
+        ("large", dataset::seq::load_normal_large()),
+    ];
+
+    for (name, data) in datasets {
+        println!("testing sort, dataset: {}", name);
+
+        let sorted_data: Vec<usize> = algorithms::divide_conquer::sort(&data)
+            .iter()
+            .map(|x| **x)
+            .collect();
+        let mut expected_data = data.clone();
+        expected_data.sort();
+
+        assert_eq!(
+            sorted_data, expected_data,
+            "sort on {} dataset has incorrect sorted result",
+            name
+        );
     }
-    assert_eq!(min, expected_min);
-    assert_eq!(max, expected_max);
-}
-
-#[test]
-#[ignore]
-fn test_find_min_max_large() {
-    let data = dataset::seq::load_normal_large();
-    let (min, max) = algorithms::divide_conquer::find_min_max(&data);
-    let mut expected_min = 0;
-    let mut expected_max = 0;
-    for (index, value) in data.iter().enumerate() {
-        if value < &data[expected_min] {
-            expected_min = index;
-        }
-        if value > &data[expected_max] {
-            expected_max = index;
-        }
-    }
-    assert_eq!(min, expected_min);
-    assert_eq!(max, expected_max);
-}
-
-#[test]
-#[ignore]
-fn test_sort_small() {
-    let data = dataset::seq::load_normal_small();
-    let sorted_data: Vec<usize> = algorithms::divide_conquer::sort(&data)
-        .iter()
-        .map(|x| **x)
-        .collect();
-    let mut expected_data = data.clone();
-    expected_data.sort();
-    assert_eq!(sorted_data, expected_data);
-}
-
-#[test]
-#[ignore]
-fn test_sort_medium() {
-    let data = dataset::seq::load_normal_medium();
-    let sorted_data: Vec<usize> = algorithms::divide_conquer::sort(&data)
-        .iter()
-        .map(|x| **x)
-        .collect();
-    let mut expected_data = data.clone();
-    expected_data.sort();
-    assert_eq!(sorted_data, expected_data);
-}
-
-#[test]
-#[ignore]
-fn test_sort_large() {
-    let data = dataset::seq::load_normal_large();
-    let sorted_data: Vec<usize> = algorithms::divide_conquer::sort(&data)
-        .iter()
-        .map(|x| **x)
-        .collect();
-    let mut expected_data = data.clone();
-    expected_data.sort();
-    assert_eq!(sorted_data, expected_data);
 }
